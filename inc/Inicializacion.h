be6384527eb285080c477f3af9be29b5f33abada
Federico Santana <fede.santana@hotmail.com>
2021-02-26 22:23:59 -0300

    Add inicializaciones
    Add:
    
    InicPLL
    Inicializacion
    Init_PWM
    Init_Systick
    InitUART0
    InitADC
    ADC_init

--- inc/Inicializacion.h
+++ inc/Inicializacion.h
@@ -1,24 +1,28 @@
-
+/*
+ * Inicializacion.h
+ *
+ *  Created on: 24 feb. 2021
+ *      Author: fer_o
+ */
 
 #ifndef INICIALIZACION_H_
 #define INICIALIZACION_H_
+typedef unsigned int uint32_t;
+typedef unsigned short uint16_t;
+typedef unsigned char uint8_t;
 
-#include "Regs_LPC176x.h"
-
-
-
-//declaracion de la inicializacion
-void InitADC(void);
-void Init_EINT0();
-void Init_PWM(void);
+void Inicializacion(void);
+void InicPLL(void);
 void Init_GPIO_PWM(void);
-void InicializarTimer0();
+void Init_PWM(void);
+void Init_Systick(void);
+void InitUART0(void);
+void InitADC(void);
 void SetDIR( uint8_t puerto , uint8_t pin , uint8_t dir );
 void SetPIN( uint8_t puerto , uint8_t pin , uint8_t estado );
 uint8_t GetPIN( uint8_t puerto , uint8_t pin , uint8_t actividad );
 void SetPINSEL( uint8_t puerto , uint8_t pin ,uint8_t funcion );
 void SetPINMODE( uint8_t port , uint8_t pin ,uint8_t modo);
-void inicializacion(void);
-int timerOver(void);
+void ADC_init(void);
 
 #endif /* INICIALIZACION_H_ */
