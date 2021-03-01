d68adb88b3ce67d6c190c200d9768e2c934035ef
Federico Santana <fede.santana@hotmail.com>
2021-03-01 16:41:12 -0300

    Remove debug and release files

--- Debug/driver/DR_Systick.d
+++
@@ -1,7 +0,0 @@
-driver/DR_Systick.o driver/DR_Systick.d: ../driver/DR_Systick.c \
- C:\Users\fedes\Documents\GitHub\servomotor-LPC-1769\inc/mqe.h \
- C:\Users\fedes\Documents\GitHub\servomotor-LPC-1769\inc/DR_tipos.h
-
-C:\Users\fedes\Documents\GitHub\servomotor-LPC-1769\inc/mqe.h:
-
-C:\Users\fedes\Documents\GitHub\servomotor-LPC-1769\inc/DR_tipos.h:
